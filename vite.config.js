608dd4bc80ea3dbe667b84bce6503af09e26e87c
Balram Singh <balram_singh08@hotmail.com>
2024-09-28 01:58:36 +0100

    setup vite config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/bobzila1202.github.io/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',  // Customize output directory
+  },
+})
